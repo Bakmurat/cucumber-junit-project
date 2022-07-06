39123e276ead927953672b3ea971c44922a2f9cf
Aysun120 <aysundamar114@gmail.com>
2022-07-05 20:45:46 -0500

    WebTableOrderStepDef class  added

--- src/test/java/com/cydeo/step_definitions/WebTableOrderStepDef.java
+++ src/test/java/com/cydeo/step_definitions/WebTableOrderStepDef.java
@@ -8,6 +8,7 @@ import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -74,24 +75,24 @@ public class WebTableOrderStepDef {
         }
 
 
-
-
     }
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+     webTableOrderPage.creditCardNumber.sendKeys(string);
 
     }
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
+     webTableOrderPage.expiryDate.sendKeys(string);
 
     }
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        webTableOrderPage.processOrderBtn.click();
     }
     @Then("user should see {string} in first row of the web table")
     public void user_should_see_in_first_row_of_the_web_table(String string) {
 
+        Assert.assertEquals();
     }
 }
